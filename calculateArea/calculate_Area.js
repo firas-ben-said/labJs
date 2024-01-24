8ed12b9aaa0d26f0cad27fa32793f081d8cd0d3d
firas-ben-said <firas.ben.said@horizon-tech.tn>
2024-01-24 17:18:40 -0500

    task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,4 +1,4 @@
-let length;
+/*let length;
 let width;
 
 function calculateArea() {
@@ -7,4 +7,17 @@ function calculateArea() {
 
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}*/
+
+let amount1;
+let amount2;
+let amount3;
+
+function calculateAmount() {
+    amount1 = parseFloat(document.getElementById('grocery1').value);
+    amount2 = parseFloat(document.getElementById('grocery2').value);
+    amount3 = parseFloat(document.getElementById('grocery3').value);
+
+    let totalAmount = amount1 + amount2 + amount3;
+    document.getElementById('result').innerText = `The Total amount is: ${totalAmount}`;
 }
